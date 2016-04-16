a379c50a734a5d80b14a02ee9ec01f4d76ac311b
James Lynch <jim@fantasyup.com>
2016-04-16 11:42:37 -0400

    Deleted wrong files

--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('angularTests', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ngMaterial', 'toastr']);
-
-})();
