8056c68e85913f5d282b66e5e20cd24fcf19d1e3
James Lynch <jameslynch@AI-R1-JL.local>
2016-04-25 12:48:03 -0500

    Polishing up Button Click Example

--- Testing-a-Button-Click/e2e/calculator.spec.js
+++ Testing-a-Button-Click/e2e/calculator.spec.js
@@ -7,18 +7,45 @@
 
      describe('CalculatorController e2e', function () {
 
-          it('should find the \"calculator-title\" element', function() {
-               browser.get('http://juliemr.github.io/protractor-demo/');
-
-               expect(element(by.class('calculator-title'))).toBeTruthy();
-
+          beforeEach(function() {
+               browser.get('http://localhost:3000/');
           })
 
+          it('should find the \"calculator-title\" element', function() {
+               expect(element(by.css('.page-title')).isPresent()).toBeTruthy();
+          });
+
           it('should not find elements that don\'t exist', function() {
-               browser.get('http://juliemr.github.io/protractor-demo/');
+               browser.get('http://localhost:3000/');
 
-               expect(element(by.class('nonexistent-element'))).toBeFalsy();
+               expect(element(by.css('nonexistent-element')).isPresent()).toBeFalsy();
 
+          });
+
+
+          it('should not have clicked text visible to start', function() {
+               expect(element(by.id('clicked-text')).isPresent()).toBeTruthy();
+               expect(element(by.id('clicked-text')).isDisplayed()).toBeFalsy();
+
+          });
+
+          it('should show the text after the button is clicked', function () {
+               element(by.css('.my-button')).click();
+               expect(element(by.id('clicked-text')).isDisplayed()).toBeTruthy();
+          });
+
+          it ('should show correct text after button is pressed once.', function() {
+               element(by.css('.my-button')).click();
+               expect(element(by.id('clicked-text')).getText()).toEqual('The button has been clicked 1 time.');
+          });
+
+          it('should show the correct text after button is clicked 5 times.', function () {
+               element(by.css('.my-button')).click();
+               element(by.css('.my-button')).click();
+               element(by.css('.my-button')).click();
+               element(by.css('.my-button')).click();
+               element(by.css('.my-button')).click();
+               expect(element(by.id('clicked-text')).getText()).toEqual('The button has been clicked 5 times.');
           })
 
      })
