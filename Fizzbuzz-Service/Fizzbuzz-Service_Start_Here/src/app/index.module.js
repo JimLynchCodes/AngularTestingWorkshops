e21d3b92c84f4e52520577f4b7bf4571cdc19b92
James Lynch <mrdotjim@gmail.com>
2016-06-07 23:20:33 -0400

    separated start here project

---
+++ Fizzbuzz-Service/Fizzbuzz-Service_Start_Here/src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('fizzbuzzService', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
