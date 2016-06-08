a2cd78e8582802081951e0c15960aa1a7069a030
James Lynch <mrdotjim@gmail.com>
2016-06-07 23:20:33 -0400

    separated start here project

---
+++ src/app/fizzbuzz-service/fizzbuzz.service.spec.js
@@ -0,0 +1,27 @@
+
+
+'use strict';
+
+describe('Fizzbuzz Service', function() {
+
+  var fizzbuzzService;
+  var $log;
+
+  beforeEach(module('fizzbuzzService'))
+  beforeEach(module('fizzbuzzService', function($provide) {
+    $provide.value(console);
+  }));
+
+
+  beforeEach(inject(function(FizzbuzzService, _$log_) {
+    fizzbuzzService = FizzbuzzService;
+    $log = _$log_;
+  }));
+
+
+  it('should...', function () {
+
+  })
+  
+
+});
